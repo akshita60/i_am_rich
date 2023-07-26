8e13d306ef24443d7f022505dfca42ed482d5769
Akshita <122316097+akshita60@users.noreply.github.com>
2023-07-26 18:11:31 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+      MaterialApp(
+          home: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title:Center(
+                child: Text('I am Rich'),
+              ),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: Center(
+              child: Image(
+                image: AssetImage('image/diamond.png'),
+
+              ),
+            ),
+
+          )
+
+      )
+  );
+}
